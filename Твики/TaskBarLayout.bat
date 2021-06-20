b7ba07ede2acb795d4ba3e18fc75fa916b2241bd
windows11help <86190960+windows11help@users.noreply.github.com>
2021-06-20 15:21:16 +0300

    Create TaskBarLayout.bat

---
+++ Твики/TaskBarLayout.bat
@@ -0,0 +1,24 @@
+@Echo off
+
+:nashalo
+Echo ============ Choose: ============
+Echo Left TaskBar - 0 (BUGS!)
+Echo Up TaskBar - 1
+Echo Right TaskBar - 2 (BUGS!)
+Echo Down TaskBar - 3
+Set /p vibor="Number: "
+
+if "%vibor%"=="0" (
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /t REG_BINARY /d 30000000feffffff0200000000000000300000002000000000000000c203000080070000e20300006000000001000000 /f
+) else if "%vibor%"=="1" (
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /t REG_BINARY /d 30000000feffffff0200000001000000300000002000000000000000c203000080070000e20300006000000001000000 /f
+) else if "%vibor%"=="2" (
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /t REG_BINARY /d 30000000feffffff0200000002000000300000002000000000000000c203000080070000e20300006000000001000000 /f
+) else if "%vibor%"=="3" (
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3" /v Settings /t REG_BINARY /d 30000000feffffff0200000003000000300000002000000000000000c203000080070000e20300006000000001000000 /f
+) else (Echo Wrong! & goto nashalo)
+
+taskkill /F /IM explorer.exe
+start explorer.exe
+goto nashalo
+pause >nule >nul
