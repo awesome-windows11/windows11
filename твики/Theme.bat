e05829eea021c807697596834c9eb4929a11100e
awesome-windows11 <86190960+awesome-windows11@users.noreply.github.com>
2021-06-23 01:06:58 +0300

    Add files via upload

---
+++ твики/Theme.bat
@@ -0,0 +1,19 @@
+@echo off
+:nashalo
+Echo ============ Choose: ============
+Echo Night Theme - 0
+Echo Light Theme - 1
+Set /p vibor="Number: "
+
+if "%vibor%"=="0" (
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
+) else if "%vibor%"=="1" (
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 1 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 1 /f
+) else (Echo Wrong! & goto nashalo)
+
+taskkill /F /IM explorer.exe
+start explorer.exe
+goto nashalo
+pause >nule >nul
