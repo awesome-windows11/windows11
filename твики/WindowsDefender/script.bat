b2d2e754702c7f9cae5a3f72ce5de57b8faba825
awesome-windows11 <86190960+awesome-windows11@users.noreply.github.com>
2021-06-27 14:24:11 +0300

    Update script.bat

--- твики/WindowsDefender/script.bat
+++ твики/WindowsDefender/script.bat
@@ -13,9 +13,12 @@ Set /p vibor="Number: "
 
 if "%vibor%"=="0" (
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SecurityHealthService" /v Start /t REG_DWORD /d 4 /f
+:: Служба которая сканирует файлы и убивает HDD
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WinDefend" /v Start /t REG_DWORD /d 4 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
 ) else if "%vibor%"=="1" (
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SecurityHealthService" /v Start /t REG_DWORD /d 2 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WinDefend" /v Start /t REG_DWORD /d 2 /f
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /f
 ) else (
 Echo Wrong! & goto nashalo
