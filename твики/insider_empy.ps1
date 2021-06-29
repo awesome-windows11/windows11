b723512f5dc7617da28d754e6f897e8cd842bb9b
awesome-windows11 <86190960+awesome-windows11@users.noreply.github.com>
2021-06-29 14:16:37 +0300

    Update insider_empy.ps1

--- твики/insider_empy.ps1
+++ твики/insider_empy.ps1
@@ -1,5 +1,5 @@
-; Исправить пустую страницу в Windows Insider
-; Download File: https://windows11.now.sh/твики/insider_empy.ps1
+# Исправить пустую страницу в Windows Insider
+# Download File: https://windows11.now.sh/твики/insider_empy.ps1
 $path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
 $value = "3"
 New-ItemProperty -Path $path -Name AllowTelemetry -Value $value -Type Dword -Force
