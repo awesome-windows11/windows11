6966b5215590d723e3678ae347a30570fefa6730
awesome-windows11 <86190960+awesome-windows11@users.noreply.github.com>
2021-06-29 16:06:39 +0300

    Delete insider_empy.ps1

--- твики/insider_empy.ps1
+++
@@ -1,6 +0,0 @@
-# Исправить пустую страницу в Windows Insider
-# Download File: https://windows11.now.sh/твики/insider_empy.ps1
-$path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
-$value = "3"
-New-ItemProperty -Path $path -Name AllowTelemetry -Value $value -Type Dword -Force
-New-ItemProperty -Path $path -Name MaxTelemetryAllowed -Value $value -Type Dword -Force
