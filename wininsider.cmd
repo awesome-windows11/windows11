3916b53af2aa4d95c56214425a4b57cc6679a1f3
awesome-windows11 <86190960+awesome-windows11@users.noreply.github.com>
2021-06-29 20:09:52 +0300

    Update wininsider.cmd

--- wininsider.cmd
+++ wininsider.cmd
@@ -84,8 +84,8 @@ goto :EOF
 
 :ADD_INSIDER_CONFIG
 rem Установите Windows 11 даже без TPM и безопасной загрузки
-reg add "KEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig" /t REG_DWORD /v BypassTPMCheck /d 1 /f
-reg add "KEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig" /t REG_DWORD /v BypassSecureBootCheck /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig" /t REG_DWORD /v BypassTPMCheck /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig" /t REG_DWORD /v BypassSecureBootCheck /d 1 /f
 
 
 rem Включаем получение инсайдерских сборок Windows 11, если ваш компьютер не соответствует минимальным критериям
