a544a7bbb4b3c612150ede73200a320995051651
awesome-windows11 <86190960+awesome-windows11@users.noreply.github.com>
2021-06-29 14:15:29 +0300

    Create insider_empy.ps1

---
+++ твики/insider_empy.ps1
@@ -0,0 +1,4 @@
+$path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
+$value = "3"
+New-ItemProperty -Path $path -Name AllowTelemetry -Value $value -Type Dword -Force
+New-ItemProperty -Path $path -Name MaxTelemetryAllowed -Value $value -Type Dword -Force
