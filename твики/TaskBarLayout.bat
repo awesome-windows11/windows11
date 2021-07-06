61b771e5fb3aa883774539acb9e0f1606826960f
awesome-windows11 <86190960+awesome-windows11@users.noreply.github.com>
2021-07-07 01:34:30 +0300

    Update TaskBarLayout.bat

--- твики/TaskBarLayout.bat
+++ твики/TaskBarLayout.bat
@@ -2,9 +2,10 @@
 
 :nashalo
 Echo ============ Choose: ============
-Echo Left TaskBar - 0 (BUGS!) ; Вызывает баги, не использовать!
+; Вызывает баги, не использовать!
+Echo Left TaskBar - 0 (BUGS!)
 Echo Up TaskBar - 1
-:: Вызывает баги, не использовать!
+; Вызывает баги, не использовать!
 Echo Right TaskBar - 2 (BUGS!)
 Echo Down TaskBar - 3
 Set /p vibor="Number: "
