e7f067cc1b82c938130d9b9c69bdf8cf62b159ae
Windows <voron5096@hotmail.com>
2021-07-22 21:53:43 +0300

    edge sync final

--- edgesync.bat
+++ edgesync.bat
@@ -9,13 +9,10 @@ Echo Sync Dysabled - 1
 Set /p vibor="Number: "
 
 if "%vibor%"=="0" (
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge" /v SyncDisabled /t REG_DWORD /d 0 /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge" /v SyncDisabled /f
 ) else if "%vibor%"=="1" (
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge" /v SyncDisabled /t REG_DWORD /d 1 /f
 ) else (Echo Wrong! & goto nashalo)
-
-taskkill /F /IM explorer.exe
-start explorer.exe
 
 goto nashalo
 
