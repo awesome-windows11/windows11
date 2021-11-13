d19a7bc189e38b1158dc95eab4e990b5af5e3a6e
awesome-windows11 <86190960+awesome-windows11@users.noreply.github.com>
2021-11-13 21:10:58 +0300

    Update remove_edge.bat

--- remove_edge.bat
+++ remove_edge.bat
@@ -4,5 +4,5 @@ for /f "tokens=1 delims=\" %%i in ('dir /B /A:D "%ProgramFiles(x86)%\Microsoft\E
 EdgeWebView\Application\%version%\Installer\setup.exe --uninstall --force-uninstall --msedgewebview --system-level --verbose-logging
 Edge\Application\%version%\Installer\setup.exe --uninstall --force-uninstall --msedge --system-level --verbose-logging
 EdgeCore\%version%\Installer\setup.exe --uninstall --force-uninstall --msedge --system-level --verbose-logging
-exit /b
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft" /v DoNotUpdateToEdgeWithChromium /t REG_DWORD /d 1 /f
+pause
