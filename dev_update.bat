271bf2d798dcc0644a08964221b725f7c4f54181
awesome-windows11 <86190960+awesome-windows11@users.noreply.github.com>
2022-02-15 22:37:28 +0300

    Delete dev_update.bat

--- dev_update.bat
+++
@@ -1,2 +0,0 @@
-:: Кароч, чтоб без переключения на DEV прилетали обновы надо удалить один из этих параметров, для этого выполнить от админа:
-reg delete "HKLM\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" /v "DeferFeatureUpdatesPeriodInDays" /f
