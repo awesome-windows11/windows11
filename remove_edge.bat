3a2cb6ac683ec02de4bfa98fe980b4cc5e776242
jestxfot <87380272+jestxfot@users.noreply.github.com>
2022-05-15 14:40:49 +0300

    Update remove_edge.bat

--- remove_edge.bat
+++ remove_edge.bat
@@ -1,4 +1,3 @@
-@echo off
 cd /d "%ProgramFiles(x86)%\Microsoft"
 for /f "tokens=1 delims=\" %%i in ('dir /B /A:D "%ProgramFiles(x86)%\Microsoft\Edge\Application" ^| find "."') do (set "version=%%i")
 EdgeWebView\Application\%version%\Installer\setup.exe --uninstall --force-uninstall --msedgewebview --system-level --verbose-logging
