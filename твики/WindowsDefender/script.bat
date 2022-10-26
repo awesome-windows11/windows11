75560a18e21ca389320a421fd289856c1656b04f
jestxfot <87380272+jestxfot@users.noreply.github.com>
2022-10-26 18:17:25 +0300

    Delete script.bat

--- твики/WindowsDefender/script.bat
+++
@@ -1,2 +0,0 @@
-reg add "HKLM\SOFTWARE\Policies\Microsoft\MRT" /v "DontOfferThroughWUAU" /t REG_DWORD /d "1" /f >nul
-reg add "HKLM\SOFTWARE\Policies\Microsoft\MRT" /v "DontReportInfectionInformation" /t REG_DWORD /d "1" /f >nul
